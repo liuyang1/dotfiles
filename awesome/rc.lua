2007cbbd1edb28c8cb732255aac5e48e6bb243a0
liuyang1 <liuyang1@mail.ustc.edu.cn>
2013-12-03 09:51:22 +0800

    add nm-applet run_once

--- awesome/rc.lua
+++ awesome/rc.lua
@@ -379,3 +379,15 @@ end)
 client.add_signal("focus", function(c) c.border_color = beautiful.border_focus end)
 client.add_signal("unfocus", function(c) c.border_color = beautiful.border_normal end)
 -- }}}
+
+function run_once(cmd)
+	findme = cmd
+	firstspace = cmd:find(" ")
+	if firstspace then
+		findme = cmd:sub(0, firstspace - 1)
+	end
+	-- check program is running, if not, then run it
+	awful.util.spawn_with_shell("pgrep -u $USER -x " .. findme .. " > dev/null || (" .. cmd .. ")")
+end
+
+run_once("nm-applet")
