13094d409475590e541188b30e587a815a03fee0
liuyang1 <liuyang1@mail.ustc.edu.cn>
2013-12-23 20:59:25 +0800

    update urxvt

---
+++ urxvt/install.sh
@@ -0,0 +1,14 @@
+#! /usr/bin/env bash
+PREFIX=~
+PWD=`pwd`
+
+installFile(){
+	# installFile $1-> $2
+	echo installFile $1
+	rm $2
+	ln -s $1 $2
+}
+
+installFile $PWD/Xresources		$PREFIX/.Xresources
+
+echo "OK"
