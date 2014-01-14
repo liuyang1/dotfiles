63c549ee978201cddbe4745a861e6342b567dcfc
liuyang1 <liuyang1@mail.ustc.edu.cn>
2014-01-14 14:06:57 +0800

    add uncrustify cfg

---
+++ uncrustify/install.sh
@@ -0,0 +1,14 @@
+#! /usr/bin/env bash
+PREFIX=~
+PWD=`pwd`
+
+installFile(){
+	# installFile $1-> $2
+	echo installFile $1
+	rm $2
+	ln -s $1 $2
+}
+
+installFile $PWD/uncrustify.cfg $PREFIX/.uncrustify.cfg
+
+echo "OK"
