fdfac817c6f80cb37786020bd08f17633a8476c8
liuyang1 <liuyang1@mail.ustc.edu.cn>
2014-01-16 17:06:34 +0800

    add Server config

---
+++ server/install.sh
@@ -0,0 +1,17 @@
+#! /usr/bin/env bash
+PREFIX=~
+PWD=`pwd`
+
+installFile(){
+	# installFile $1-> $2
+	echo installFile $1
+	rm $2
+	ln -s $1 $2
+}
+
+installFile $PWD/dircolors $PREFIX/.dircolors
+installFile $PWD/bashrc $PREFIX/.bashrc
+installFile $PWD/bash_profile $PREFIX/.bash_profile
+installFile $PWD/inputrc $PREFIX/.inputrc
+
+echo "OK"
