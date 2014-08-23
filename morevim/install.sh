08f8e11814fbf55c15b528f637766162103da441
liuyang1 <liuyang1@mail.ustc.edu.cn>
2014-08-23 19:45:47 +0800

    update install.sh for morevim

--- morevim/install.sh
+++ morevim/install.sh
@@ -1,11 +1,11 @@
 #! /usr/bin/env bash
-PREFIX=~/testpath/
+PREFIX=~/
 PWD=`pwd`
 
 installFile(){
 	# installFile $1-> $2
 	echo installFile $1 $2
-	rm $2
+	rm -rf $2
 	ln -s $1 $2
 }
 
@@ -13,3 +13,4 @@ installFile $PWD/root.vim $PREFIX/.vimrc
 installFile $PWD $PREFIX/.vim
 
 echo "OK"
+echo "must run BundleUpdate under vim"
