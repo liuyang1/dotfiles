e198628abea3b57fdef03e1e49d2f24705ecc4a7
liuyang <ly@marvell.com>
2014-09-11 20:15:48 +0800

    update zsh install script

--- zsh/install.sh
+++ zsh/install.sh
@@ -13,6 +13,17 @@ installFile $PWD/zshrc          $PREFIX/.zshrc
 installFile $PWD/aliases        $PREFIX/.aliases
 installFile $PWD/zshenv         $PREFIX/.zshenv
 installFile $PWD/zsh_local      $PREFIX/.zsh_local
-installFile $PWD/myagnoster.zsh-theme       $PREFIX/.oh-my-zsh/themes/myagnoster.zsh-theme
+
+ZSH=$PREFIX/.oh-my-zsh
+ZSH_THEME_PATH=$ZSH/theme
+ZSH_PLUGIN_PATH=$ZSH/custom/plugins
+if [[ ! -d $ZSH ]];
+then
+    echo "oh my zsh theme not in defualt path! setting it yourself"
+else
+    installFile $PWD/myagnoster.zsh-theme       $PREFIX/.oh-my-zsh/themes/myagnoster.zsh-theme
+    cd $ZSH_PLUGIN_PATH
+    git clone git://github.com/zsh-users/zsh-syntax-highlighting.git
+fi
 
 echo "OK"
