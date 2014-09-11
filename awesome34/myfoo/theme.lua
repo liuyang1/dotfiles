2ef091e017f4e1601af26c4f4abb42b8164676ff
liuyang1 <liuyang1@mail.ustc.edu.cn>
2014-09-11 23:29:22 +0000

    add awesome34 myfoo theme

---
+++ awesome34/myfoo/theme.lua
@@ -0,0 +1,107 @@
+-- foo, high contrast awesome3 theme, by rob
+
+--{{{ Main
+require("awful.util")
+
+theme = {}
+
+home          = os.getenv("HOME")
+config        = awful.util.getdir("config")
+shared        = "/usr/share/awesome"
+if not awful.util.file_readable(shared .. "/icons/awesome16.png") then
+    shared    = "/usr/share/local/awesome"
+end
+sharedicons   = shared .. "/icons"
+sharedthemes  = shared .. "/themes"
+themes        = config .. "/themes"
+themename     = "/myfoo"
+if not awful.util.file_readable(themes .. themename .. "/theme.lua") then
+       themes = sharedthemes
+end
+themedir      = themes .. themename
+
+wallpaper1    = themedir .. "/background.jpg"
+wallpaper2    = themedir .. "/background.png"
+wallpaper3    = sharedthemes .. "/zenburn/zenburn-background.png"
+wallpaper4    = sharedthemes .. "/default/background.png"
+wpscript      = home .. "/.wallpaper"
+
+if awful.util.file_readable(wallpaper1) then
+	theme.wallpaper_cmd = { "awsetbg -c" .. wallpaper1 }
+elseif awful.util.file_readable(wallpaper2) then
+	theme.wallpaper_cmd = { "awsetbg " .. wallpaper2 }
+elseif awful.util.file_readable(wpscript) then
+	theme.wallpaper_cmd = { "sh " .. wpscript }
+elseif awful.util.file_readable(wallpaper3) then
+	theme.wallpaper_cmd = { "awsetbg " .. wallpaper3 }
+else
+	theme.wallpaper_cmd = { "awsetbg " .. wallpaper4 }
+end
+
+thinkpaper = "/home/liuy/.config/awesome/wall.jpg"
+theme.wallpaper_cmd = { "awsetbg -a " .. thinkpaper}
+-- theme.wallpaper_cmd = { "feh --bg-fill " .. thinkpaper}
+-- wpscript2     = themedir .. "/niceandclean.sh"
+-- theme.wallpaper_cmd = { "sh " .. wpscript2 }
+
+if awful.util.file_readable(config .. "/vain/init.lua") then
+    theme.useless_gap_width  = "3"
+end
+--}}}
+
+theme.font    = "Ubuntu Mono 12"
+--theme.font      = "Terminus Bold 9"
+--theme.font    = "Consolas Bold 9"
+
+theme.bg_normal     = "#333333A0"
+theme.bg_focus      = "#1279bfA0"
+theme.bg_urgent     = "#00ff00"
+
+theme.fg_normal     = "#999999"
+theme.fg_focus      = "#ffffff"
+theme.fg_urgent     = "#111111"
+
+theme.border_width  = 2
+theme.border_normal = "#222222"
+theme.border_focus  = "#1279bf"
+
+--[[theme.bg_normal = "#000000"
+theme.bg_focus  = "#2e8b57"
+theme.bg_urgent = "#a52a2a"
+
+theme.fg_normal = "#00ffff"
+theme.fg_focus  = "#000000"
+theme.fg_urgent = "#000000"
+
+theme.border_width  = 2
+theme.border_normal = "#000000"
+theme.border_focus  = "#2e8b57"]]
+
+theme.taglist_squares_sel         = themedir .. "/tasklist_f.png"
+theme.taglist_squares_unsel       = themedir .. "/tasklist_u.png"
+theme.tasklist_floating_icon      = themedir .. "/floating.png"
+
+theme.titlebar_close_button_normal = sharedthemes .. "/default/titlebar/close.png"
+theme.titlebar_close_button_focus  = sharedthemes .. "/default/titlebar/closer.png"
+
+theme.menu_submenu_icon = sharedthemes .. "/default/submenu.png"
+theme.menu_height   = "24"
+theme.menu_width    = "200"
+
+theme.awesome_icon = sharedicons .. "/awesome16.png"
+-- {{{ Layout
+theme.layout_tile       = "/usr/share/awesome/themes/zenburn/layouts/tile.png"
+theme.layout_tileleft   = "/usr/share/awesome/themes/zenburn/layouts/tileleft.png"
+theme.layout_tilebottom = "/usr/share/awesome/themes/zenburn/layouts/tilebottom.png"
+theme.layout_tiletop    = "/usr/share/awesome/themes/zenburn/layouts/tiletop.png"
+theme.layout_fairv      = "/usr/share/awesome/themes/zenburn/layouts/fairv.png"
+theme.layout_fairh      = "/usr/share/awesome/themes/zenburn/layouts/fairh.png"
+theme.layout_spiral     = "/usr/share/awesome/themes/zenburn/layouts/spiral.png"
+theme.layout_dwindle    = "/usr/share/awesome/themes/zenburn/layouts/dwindle.png"
+theme.layout_max        = "/usr/share/awesome/themes/zenburn/layouts/max.png"
+theme.layout_fullscreen = "/usr/share/awesome/themes/zenburn/layouts/fullscreen.png"
+theme.layout_magnifier  = "/usr/share/awesome/themes/zenburn/layouts/magnifier.png"
+theme.layout_floating   = "/usr/share/awesome/themes/zenburn/layouts/floating.png"
+-- }}}
+
+return theme
