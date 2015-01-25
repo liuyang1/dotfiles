b0a47c3d351898aad12ca15ea2ea1f8951f71659
liuyang1 <liuyang1@mail.ustc.edu.cn>
2015-01-25 19:30:02 +0800

    update cmstat.py to support multi-repo

--- gist/cmstat.py
+++ gist/cmstat.py
@@ -3,19 +3,19 @@ import sh
 from collections import namedtuple
 import os
 import itertools
-git = sh.git.bake()
 
 
 NumStat = namedtuple('NumStat', ['insert', 'delete', 'filename'])
 
 
-def getCommit(commit):
+def getCommit(cwd, commit):
     """get commit message
     --no-pager: stop pager which block stdout
     -n 1: only show one commit
     (not use ^.. style, this will fail when not have prior commit)
     --color=never: not colorful output
     """
+    git = sh.git.bake(_cwd=cwd)
     opt = ('--numstat', '-n 1', '--color=never', '--pretty=%H')
     return git('--no-pager', 'log', commit, *opt)
 
@@ -36,12 +36,13 @@ def parseNumStat(cm):
     return ret
 
 
-def getStatLst():
-    cmlst = git('rev-list', 'HEAD', '--after=2014-01-01', '--author=liuyang')
+def getStatLst(cwd):
+    git = sh.git.bake(_cwd=cwd)
+    cmlst = git('rev-list', 'HEAD', '--after=2014-12-31', '--author=liuyang')
     shalst = cmlst.split()
     stlst = []
     for sha in shalst:
-        cm = getCommit(sha)
+        cm = getCommit(cwd, sha)
         ret = parseNumStat(cm)
         stlst.extend(ret)
     return stlst
@@ -56,18 +57,36 @@ def groupFileExt(numst):
         return ret[1]
 
 
-def main():
-    stlst = getStatLst()
+def statRepo(cwd):
+    stlst = getStatLst(cwd)
     a = sum([st.insert for st in stlst])
     b = sum([st.delete for st in stlst])
     print(a, b, a + b)
+    ret = (a, b, a + b)
     stlst = sorted(stlst, key=groupFileExt)
     for ext, g in itertools.groupby(stlst, groupFileExt):
         g = list(g)
         aa = sum([st.insert for st in g])
         bb = sum([st.delete for st in g])
         print(ext, aa, bb, aa + bb)
+    return ret
 
 
 if __name__ == "__main__":
-    main()
+    import argparse
+    parser = argparse.ArgumentParser(
+        description='git stat accumulate from specific date')
+    parser.add_argument('dirs', type=str, nargs='*',
+                        help="repo dirs, default to current dir")
+    args = parser.parse_args()
+    dirs = args.dirs
+    if len(dirs) == 0:
+        statRepo(os.getcwd())
+    else:
+        ret = []
+        for d in dirs:
+            ret.append(statRepo(d))
+        cum = map(sum, zip(* ret))
+        for i in cum:
+            print(i, end=" ")
+        print('')
