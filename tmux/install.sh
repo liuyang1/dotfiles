b72bbfe045dde992b086ac0006fcf32a5dde0770
liuyang1 <liuyang1@mail.ustc.edu.cn>
2015-04-30 18:38:44 +0800

    update tmux

--- tmux/install.sh
+++ tmux/install.sh
@@ -9,19 +9,25 @@ while true; do
 [1]:after  1.9a,
  > " val
     case $val in
-        [0]* ) TMUXConf=tmux.18.conf; break;;
-        [1]* ) TMUXConf=tmux.19.conf; break;;
+        [0]* ) TMUXConf=tmux.18.conf;
+            ROOTConf=root.18.conf;
+            break;;
+        [1]* ) TMUXConf=tmux.19.conf;
+            ROOTConf=root.19.conf;
+            break;;
         * ) echo "Please answer 0 or 1.";;
     esac
 done
 
-installFile root.conf "$PREFIX"/.tmux.conf
+installFile $TMUXConf "$PREFIX"/.tmux.conf
 installFile "$PWD" "$PREFIX"/.tmux
 installFile $TMUXConf "$PREFIX/.tmux/tmux.version.conf"
 
-[[ -d "plugins" ]] || mkdir plugins
+if [[ "$TMUXConf" == "tmux.19.conf" ]]; then
+    [[ -d "plugins" ]] || mkdir plugins
 
-cd plugins
-[[ -d "tpm" ]] || git clone https://github.com/tmux-plugins/tpm
+    cd plugins
+    [[ -d "tpm" ]] || git clone https://github.com/tmux-plugins/tpm
+fi
 
 echo "OK"
