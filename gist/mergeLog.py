d4d6a6ddee42ae3773b0f2e58b2028cdf6432d99
liuyang1 <liuyang1@mail.ustc.edu.cn>
2015-08-04 09:01:09 +0800

    opt mergeLog.py simpler

--- gist/mergeLog.py
+++ gist/mergeLog.py
@@ -8,50 +8,37 @@ def getLineNo(l):
     return int(l[0:idx])
 
 
+def Logger(infile):
+    f = fileinput.FileInput(infile)
+    for l in f:
+        yield l, getLineNo(l)
+    f.close()
+
+
 def mergeLog(ins, out):
-    # f0 = open(ins[0])
-    # f1 = open(ins[1])
-    f0 = fileinput.input(ins[0])
-    f1 = fileinput.input(ins[1])
-    l0 = f0.readline()
-    l1 = f1.readline()
-    n0 = getLineNo(l0)
-    n1 = getLineNo(l1)
-    notend = True
-    while notend:
-        while n1 == 0 or n0 < n1:
-            print(l0.strip())
-            l0 = f0.readline()
-            if l0 == "":
-                n0 = 0
-                break
-            n0 = getLineNo(l0)
-        if n0 == 0 and n1 == 0:
+    f0, f1 = [Logger(i) for i in ins]
+    l1, n1 = next(f1)
+    while True:
+        # this dynamic iterate, cannot run on FOR, as it bind iterator.
+        # this style could dynamic switch between Logger(ins[0), Logger(ins[1])
+        try:
+            l0, n0 = next(f0)
+        except:
             break
-        while n0 == n1:
-            print(l0.strip())
-            l0 = f0.readline()
-            n0 = getLineNo(l0)
-            l1 = f1.readline()
-            n1 = getLineNo(l1)
-        if n0 == 0 and n1 == 0:
-            break
-        while n0 == 0 or n0 > n1:
-            print(l1.strip())
-            l1 = f1.readline()
-            if l1 == "":
-                n1 = 0
-                break
-            n1 = getLineNo(l1)
-        if n0 == 0 and n1 == 0:
-            break
-    f0.close()
-    f1.close()
+        # eat repeat line
+        if n0 == n1:
+            continue
+        # exchange, always output as min lineno l0
+        if n0 > n1:
+            l0, l1, n0, n1, f0, f1 = l1, l0, n1, n0, f1, f0
+        print(l0),
+    for l, _ in f1:
+        print(l),
 
 
 if __name__ == "__main__":
     from argparse import RawTextHelpFormatter, ArgumentParser
-    desc = """read greped log from file or STDIN, merge them back to STDOUT"""
+    desc = "read greped log from file or STDIN, merge them back to STDOUT"
     parser = ArgumentParser(
         description=desc, formatter_class=RawTextHelpFormatter)
     parser.add_argument('inlog', nargs=2, help='input log file, stdin "-"')
