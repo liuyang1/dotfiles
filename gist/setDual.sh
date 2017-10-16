5a791e0a16291b3feafb75bce644ba2ab3806939
liuyang1 <liuyang1@mail.ustc.edu.cn>
2017-10-16 08:53:44 +0800

    update setDual.sh

--- gist/setDual.sh
+++ gist/setDual.sh
@@ -1,2 +1,4 @@
 #! /usr/bin/env bash
-xrandr --output VGA-0 --left-of LVDS --auto
+# xrandr --output VGA-0 --left-of LVDS --auto
+xrandr --output VGA-1 --auto --left-of DP-2 --auto
+xrandr --output DP-2 --mode 3840x2160
