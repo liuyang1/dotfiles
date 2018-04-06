eb376ee04ac5329352906eb1f6b1c2b389c02189
liuyang1 <liuyang1@mail.ustc.edu.cn>
2018-04-06 20:09:09 +0800

    git: reset-author, userGit.sh

--- git/install.sh
+++ git/install.sh
@@ -12,5 +12,6 @@ installFile(){
 installFile $PWD/gitconfig			$PREFIX/.gitconfig
 installFile $PWD/gitignore_global	$PREFIX/.gitignore_global
 installFile $PWD/rmGitFile.sh       ~/.local/bin/rmGitFile.sh
+installFile $PWD/userGit.sh         ~/.locla/bin/userGit.sh
 
 echo "OK"
---
+++ git/userGit.sh
@@ -0,0 +1,2 @@
+git config --local user.name "liuyang1"
+git config --local user.email "liuyang1@mail.ustc.edu.cn"
