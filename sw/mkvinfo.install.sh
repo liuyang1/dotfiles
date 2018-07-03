4f318d7880afaaef75dd5b3574209273341e1498
liuyang1 <liuyang1@mail.ustc.edu.cn>
2018-07-03 10:17:31 +0800

    sw: add mkvinfo.install.sh

---
+++ sw/mkvinfo.install.sh
@@ -0,0 +1,10 @@
+sudo apt-get install libogg-dev
+sudo apt-get install libvorbis
+sudo apt-get install libvorbis-dev
+sudo apt-get install libboost-all-dev
+sudo apt-get install docbook-xsl
+sudo apt-get install xsltproc
+sudo apt-get install rake
+./configure
+rake -j4
+# sudo rake install
