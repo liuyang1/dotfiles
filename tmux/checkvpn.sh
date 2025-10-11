b7bdfb230615328c2f567da6b10a3864507ec140
liuyang1 <liuyang1@mail.ustc.edu.cn>
2025-10-11 17:10:05 +0800

    tmux: update
    Signed-off-by: liuyang1 <liuyang1@mail.ustc.edu.cn>

--- tmux/checkvpn.sh
+++ tmux/checkvpn.sh
@@ -1,5 +1,6 @@
+# updat eto use utun4 to check openvpn client
 check_vpn() {
-    if ifconfig | grep -q "tun0"; then
+    if ifconfig | grep -q "utun4"; then
         echo "#[fg=green]VPN:Wired#[default]"
     else
         echo "#[fg=red]VPN:Broken#[default]"
